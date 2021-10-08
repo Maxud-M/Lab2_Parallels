39750a32e329584c032cba37409a0872e5138c14
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-08 19:55:47 +0300

    Scripted auto-commit on change (2021-10-08 19:55:47) by gitwatch.sh

--- src/main/java/AirPortWritable.java
+++ src/main/java/AirPortWritable.java
@@ -1,2 +1,17 @@
-public class AirPortWritable {
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirPortWritable implements Writable {
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
