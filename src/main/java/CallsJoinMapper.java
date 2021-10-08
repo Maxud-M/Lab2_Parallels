bc19d4ff69268ea98f222dad11ca0c79d4ff7858
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-08 11:51:21 +0300

    Scripted auto-commit on change (2021-10-08 11:51:20) by gitwatch.sh

--- src/main/java/CallsJoinMapper.java
+++ src/main/java/CallsJoinMapper.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class CallsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        //It's my code
+        //It's my code tatatatatata
     }
 
 }
