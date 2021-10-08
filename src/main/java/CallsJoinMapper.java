5bcac2fe14d78729ca73ee5dc39376cb7abf3ffe
Maksud Maksudov <evistix28@Maksuds-MacBook-Air.local>
2021-10-08 10:23:18 +0300

    Scripted auto-commit on change (2021-10-08 10:23:18) by gitwatch.sh

--- src/main/java/CallsJoinMapper.java
+++ src/main/java/CallsJoinMapper.java
@@ -1,7 +1,10 @@
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class CallsJoinMapper extends Mapper {
+import java.io.IOException;
+
+public class CallsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
-    
-}
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
