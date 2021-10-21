0cdc6ecf8f712d723482eed64a0cdd04e02f4a2d
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-21 23:11:06 +0300

    Scripted auto-commit on change (2021-10-21 23:11:06) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/FlightsJoinMapper.java
+++ src/main/java/ru/evistix28/parallels/FlightsJoinMapper.java
@@ -9,8 +9,7 @@ import java.io.IOException;
 public class FlightsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        FlightWritable flight = new FlightWritable(value);
-        context.write(new TextPair(key.toString(), "0"), new Text(flight.toString()));
+
     }
 
 }
