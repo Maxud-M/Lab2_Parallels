c3f17850572da17b1bb4b032eae74879042fa09c
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-21 21:32:30 +0300

    Scripted auto-commit on change (2021-10-21 21:32:30) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/JoinReducer.java
+++ src/main/java/ru/evistix28/parallels/JoinReducer.java
@@ -1,7 +1,21 @@
 package ru.evistix28.parallels;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class JoinReducer extends Reducer {
+import java.io.IOException;
+import java.util.Iterator;
 
+public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
+    @Override
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
+            IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text systemInfo = new Text(iter.next());
+        while (iter.hasNext()) {
+            Text call = iter.next();
+            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
+            context.write(key.getFirst(), outValue);
+        }
+    }
 }
