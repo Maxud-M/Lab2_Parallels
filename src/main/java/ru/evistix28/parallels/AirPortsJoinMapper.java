5f788948c2a9da98687524c470968169149276d0
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-21 23:27:13 +0300

    Scripted auto-commit on change (2021-10-21 23:27:13) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/AirPortsJoinMapper.java
+++ src/main/java/ru/evistix28/parallels/AirPortsJoinMapper.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class AirPortsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        TextPair keys = new TextPair();
         AirPortsJoinMapper airports = new AirPortsJoinMapper(value);
     }
 }
