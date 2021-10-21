da9d9cb12691c1c547503c1b3c69e40c5b2f645b
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-21 21:13:48 +0300

    Scripted auto-commit on change (2021-10-21 21:13:48) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/TextPair.java
+++ src/main/java/ru/evistix28/parallels/TextPair.java
@@ -1,4 +1,26 @@
 package ru.evistix28.parallels;
 
+import org.apache.hadoop.io.RawComparator;
+import org.apache.hadoop.io.file.tfile.RawComparable;
+import org.apache.hadoop.mapreduce.Partitioner;
+
 public class TextPair {
+    public class FirstPartitioner extends Partitioner {
+        @Override
+        public int getPartition(Object o, Object o2, int i) {
+            return 0;
+        }
+    }
+    public class FirstComparator implements RawComparator {
+        @Override
+        public int compare(Object o1, Object o2) {
+            return 0;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return false;
+        }
+
+    }
 }
