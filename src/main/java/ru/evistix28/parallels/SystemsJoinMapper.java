e64e8040efc16a68cf62f1935c7d214dcb5ab345
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-21 21:27:58 +0300

    Scripted auto-commit on change (2021-10-21 21:27:58) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/SystemsJoinMapper.java
+++ src/main/java/ru/evistix28/parallels/SystemsJoinMapper.java
@@ -2,6 +2,11 @@ package ru.evistix28.parallels;
 
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class SystemsJoinMapper extends Mapper {
+import java.io.IOException;
 
+public class AirPortsJoinMapper extends Mapper {
+    @Override
+    protected void map(Object key, Object value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
