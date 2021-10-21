3a5e628f8f14defc3e4aa9884a81a335169954c1
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-21 22:28:12 +0300

    Scripted auto-commit on change (2021-10-21 22:28:12) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/TextPairComparable.java
+++ src/main/java/ru/evistix28/parallels/TextPairComparable.java
@@ -1,2 +1,24 @@
-package ru.evistix28.parallels;public class TextPairComparable {
+package ru.evistix28.parallels;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class TextPairComparable implements WritableComparable {
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
