0f8c7d15c7923c9e72fc8b23730726d6618f0e9f
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-22 19:44:07 +0300

    Scripted auto-commit on change (2021-10-22 19:44:07) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/AirPortReader.java
+++ src/main/java/ru/evistix28/parallels/AirPortReader.java
@@ -1,11 +1,28 @@
 package ru.evistix28.parallels;
 
+import org.apache.hadoop.io.Text;
+
 public class AirPortReader {
     public static final int DEST_AIRPORT_ID_POS = 0;
     public static final int AIRPORT_NAME_POS = 1;
+    public static final int DATA_SEPARATE;
 
-    private int aeroportID;
-    private Text aer
+    private final int airportID;
+    private final Text airportName;
 
-    public AirPortReader()
+    public AirPortReader(int id, Text name) {
+        airportID = id;
+        airportName = name;
+    }
+
+    public int getKey() {return airportID;}
+    public Text getAirportName() {return airportName;}
+
+    public AirPortReader(String value) {
+        String[] dataPair = value.split(",", 1);
+    }
+
+
+
+
 }
