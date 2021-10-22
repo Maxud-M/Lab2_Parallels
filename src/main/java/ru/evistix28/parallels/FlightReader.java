cb640c29bb63ef94727386fea5168649751ebb86
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-22 21:00:18 +0300

    Scripted auto-commit on change (2021-10-22 21:00:18) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/FlightReader.java
+++ src/main/java/ru/evistix28/parallels/FlightReader.java
@@ -1,5 +1,10 @@
 package ru.evistix28.parallels;
 
+
+import sun.plugin2.message.Message;
+
+import java.io.IOException;
+
 public class FlightReader {
 
     public static final int DEST_AIRPORT_ID_POS = 14;
@@ -11,10 +16,10 @@ public class FlightReader {
     public int getKey() {return airportID;}
     public float getFlightDelay() {return flightDelay;}
 
-    public FlightReader(String value) {
+    public FlightReader(String value) throws IOException {
         String[] data = value.split(Constants.DATA_SEPARATOR);
         airportID = Integer.parseInt(data[DEST_AIRPORT_ID_POS]);
-        if(data[FLIGHT_DELAY_POS] != Constants.EMPTY_STRING)
+        if (data[FLIGHT_DELAY_POS] != Constants.EMPTY_STRING)
             flightDelay = Float.parseFloat(data[FLIGHT_DELAY_POS]);
         else
             flightDelay = Constants.ZERO_FLOAT;
