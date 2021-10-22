c206a3ea2dc43713cb3a74f2d856de216e578165
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-22 15:43:36 +0300

    Scripted auto-commit on change (2021-10-22 15:43:36) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/AirPortWritable.java
+++ src/main/java/ru/evistix28/parallels/AirPortWritable.java
@@ -1,16 +1,20 @@
 package ru.evistix28.parallels;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 
 import java.io.*;
 
 public class AirPortWritable implements Writable {
 
+    public AirPortWritable(Text value) {
+        BufferedReader csvReader = new BufferedReader;
+    }
+
 
 
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-
     }
 
     @Override
