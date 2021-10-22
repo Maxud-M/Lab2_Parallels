089529710668496842ae0c34227199a7763e138e
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-22 16:47:38 +0300

    Scripted auto-commit on change (2021-10-22 16:47:38) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/AirPortsJoinMapper.java
+++ src/main/java/ru/evistix28/parallels/AirPortsJoinMapper.java
@@ -8,14 +8,13 @@ import java.io.IOException;
 
 public class AirPortsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
 
-    String splitBy
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String val = value.toString();
         String[] data = val.split("\n");
         for(int i = 1; i < data.length; ++i) {
-            String[] raw = data[i].split(",");
+            String[] raw = data[i].split(",", 1);
             TextPair keyOut = new TextPair(Integer.parseInt(raw[0].replaceAll("\"", "")), 0);
             Text valueOut = new Text(raw[1].replaceAll("\"", ""));
             context.write(keyOut, valueOut);
