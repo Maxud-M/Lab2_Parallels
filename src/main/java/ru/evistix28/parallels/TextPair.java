22603a5e0d00d4aaeff865bb64444d812a073f7e
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-23 00:12:40 +0300

    Scripted auto-commit on change (2021-10-23 00:12:40) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/TextPair.java
+++ src/main/java/ru/evistix28/parallels/TextPair.java
@@ -59,7 +59,7 @@ public class TextPair implements WritableComparable<TextPair> {
 
         @Override
         public int getPartition(TextPair key, Text value, int numReduceTasks) {
-            return key.airportId % numReduceTasks;
+            return key.getKey() % numReduceTasks;
         }
     }
 
