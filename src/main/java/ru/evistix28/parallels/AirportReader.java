c1d9f518fe0cee4993db473a4ee03b45aef5e694
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-22 21:31:32 +0300

    Scripted auto-commit on change (2021-10-22 21:31:32) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/AirportReader.java
+++ src/main/java/ru/evistix28/parallels/AirportReader.java
@@ -19,7 +19,6 @@ public class AirportReader {
 
     public AirportReader(String value) {
         String[] dataPair = value.split(Constants.DATA_SEPARATOR, LIMIT_FOR_SPLIT);
-        System.out.println(dataPair[DEST_AIRPORT_ID_POS]);
         airportID = Integer.parseInt(dataPair[DEST_AIRPORT_ID_POS].replaceAll(Constants.QUOTES, Constants.EMPTY_STRING));
         airportName = new Text(dataPair[AIRPORT_NAME_POS].replaceAll(Constants.QUOTES, Constants.EMPTY_STRING));
     }
