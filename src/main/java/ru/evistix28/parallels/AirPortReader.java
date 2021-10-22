375effe1e8810de25319a79d060e2f2df0fa47a3
Maxud-M <maksud.maksudov.00@mail.ru>
2021-10-22 19:38:29 +0300

    Scripted auto-commit on change (2021-10-22 19:38:29) by gitwatch.sh

--- src/main/java/ru/evistix28/parallels/AirPortReader.java
+++ src/main/java/ru/evistix28/parallels/AirPortReader.java
@@ -2,5 +2,6 @@ package ru.evistix28.parallels;
 
 public class AirPortReader {
     public static final int DEST_AIRPORT_ID_POS = 0;
-    public static final int 
+    public static final int AIRPORT_NAME_POS = 1;
+    
 }
